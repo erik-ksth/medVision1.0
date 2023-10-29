68db6638c9212b9bdc0915dbf79ff1c0e9e23911
Erik Hein <erikhein@Eriks-MacBook-Air.local>
2023-10-29 08:18:11 -0700

    build: photo uploading 2.0

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,13 @@ import { getStorage } from "firebase/storage";
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+// var admin = require("firebase-admin");
+
+// var serviceAccount = require("./med-vision.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,37 +1,87 @@
 import React from "react";
+import axios from "axios";
 import { useState, useEffect } from "react";
-import { ref, uploadBytes, getDownloadURL, listAll } from "firebase/storage";
+import { ref, uploadBytesResumable, getDownloadURL, listAll } from "firebase/storage";
 import { storage } from "../firebase";
 import { v4 } from "uuid";
+// import { storage } from "./firebaseConfig";
 
 import Button from "@mui/material/Button";
 import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
 import UploadIcon from "@mui/icons-material/Upload";
 
 function Home() {
-  const [imageUpload, setImageUpload] = useState(null);
-  const [imageUrls, setImageUrls] = useState([]);
+  const [file, setFile] = useState("");
+ 
+    // progress
+    const [percent, setPercent] = useState(0);
+ 
+    // Handle file upload event and update state
+    function handleChange(event) {
+        setFile(event.target.files[0]);
+    }
+ 
+    const handleUpload = () => {
+        if (!file) {
+            alert("Please upload an image first!");
+        }
+ 
+        const storageRef = ref(storage, `/files/${file.name}`);
+ 
+        // progress can be paused and resumed. It also exposes progress updates.
+        // Receives the storage reference and the file to upload.
+        const uploadTask = uploadBytesResumable(storageRef, file);
+ 
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const percent = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+ 
+                // update progress
+                setPercent(percent);
+            },
+            (err) => console.log(err),
+            () => {
+                // download url
+                getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+                    console.log(url);
+                });
+            }
+        );
+    };
 
-  const imagesListRef = ref(storage, "images/");
-  const uploadFile = () => {
-    if (imageUpload == null) return;
-    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-    uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        setImageUrls((prev) => [...prev, url]);
-      });
-    });
-  };
+  // const [imageUpload, setImageUpload] = useState(null);
+  // const [imageUrls, setImageUrls] = useState([]);
+  // let objectName = " ";
 
-  useEffect(() => {
-    listAll(imagesListRef).then((response) => {
-      response.items.forEach((item) => {
-        getDownloadURL(item).then((url) => {
-          setImageUrls((prev) => [...prev, url]);
-        });
-      });
-    });
-  }, []);
+  // const imagesListRef = ref(storage, "images/");
+  // const uploadFile = () => {
+  //   if (imageUpload == null) return;
+  //   const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+  //   uploadBytes(imageRef, imageUpload).then((snapshot) => {
+  //     getDownloadURL(snapshot.ref).then((url) => {
+  //       setImageUrls((prev) => [...prev, url]);
+  //       console.log("Object Name: ", objectName);
+
+  //       axios.post('api/process-image', {
+  //         objectName: objectName
+  //       })
+  //     });
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   listAll(imagesListRef).then((response) => {
+  //     response.items.forEach((item) => {
+  //       getDownloadURL(item).then((url) => {
+  //         setImageUrls((prev) => [...prev, url]);
+  //       });
+  //     });
+  //   });
+  // }, []);
+
   return (
     <body class="grid justify-items-center w-full bg-red-400">
       <div class="w-96 bg-blue-400 grid justify-items-center gap-y-32">
@@ -50,10 +100,13 @@ function Home() {
               id="captureImg"
               capture="user"
               accept="image/*"
-              onChange={(event) => {
-                setImageUpload(event.target.files[0]);
-                uploadFile();
-              }}
+              onChange={handleChange}
+              // onChange={(event) => {
+              //   handleChange();
+              //   uploadFile();
+              //   objectName = event.target.files[0].name;
+              //   console.log(event.target.files[0].name);
+              // }}
             ></input>
           </Button>
         </div>
@@ -70,13 +123,18 @@ function Home() {
               type="file"
               id="uploadImg"
               accept="image/png, image/jpeg, image/heic"
-              onChange={(event) => {
-                setImageUpload(event.target.files[0]);
-              }}
-              onClick={uploadFile}
+              onChange={handleChange}
+              // onChange={(event) => {
+              //   handleChange();
+              //   uploadFile();
+              //   objectName = event.target.files[0].name;
+              //   console.log(event.target.files[0].name);
+              // }}
             ></input>
           </Button>
+
         </div>
+          <Button variant="contained" onClick={handleUpload}>Upload to Firebase</Button>
       </div>
     </body>
   );
